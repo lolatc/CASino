a9b843412fa3899b9a0c5c148bdf8aca13b498d7
wyh770406 <wyh@qq.com>
2017-04-05 16:09:54 -0700

    for testing

--- lib/casino/engine.rb
+++ lib/casino/engine.rb
@@ -9,16 +9,14 @@ module CASino
     rake_tasks { require 'casino/tasks' }
 
     initializer :yaml_configuration do |app|
+      puts "hhhhh000000"
       apply_yaml_config load_file('config/cas.yml')
     end
 
     private
     def apply_yaml_config(yaml)
-      puts Rails.env
       cfg = (YAML.load(ERB.new(yaml).result)||{}).fetch(Rails.env, {})
       cfg.each do |k,v|
-        puts k
-        puts v
         value = if v.is_a? Hash
           CASino.config.fetch(k.to_sym,{}).merge(v.symbolize_keys)
         else
